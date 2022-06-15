f6fefcf910b124c06e044a7b4d1641bb4b6baf9a
Baisal <baisalsatylgam@gmail.com>
2022-06-15 20:05:01 +0600

    filters for blogs and admin were maden

--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Blog
+from django.db.models import Q
+
 
 # Create your views here.
 def blog_list(request):
+    q = request.GET.get('q', '')
+    q_user = request.GET.get('q_user', '')
     ordering = request.GET.get('ordering', '-id')
     blogs = Blog.objects.all()
+    if q or q_user:
+        blogs = Blog.objects.filter((Q(title__icontains=q) | Q(text__icontains=q)) & Q(author__username__icontains=q_user))
     if ordering == 'id':
         blogs = blogs.order_by('id')
         ordering = '-id'
@@ -14,7 +20,9 @@ def blog_list(request):
         ordering = 'id'
     context = {
         'blogs': blogs,
-        'ordering': ordering
+        'ordering': ordering,
+        'q': q,
+        'q_user': q_user
     }
     return render(request, 'blogs/blog_list.html', context)
 
