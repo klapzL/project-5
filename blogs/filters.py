acb4ea2d9b148bb3312417298fb1879847fefad4
Baisal <baisalsatylgam@gmail.com>
2022-06-29 20:04:53 +0600

    django_filters was done

--- blogs/filters.py
+++ blogs/filters.py
@@ -5,10 +5,8 @@ from .models import Blog, CATEGORIES_CHOICES
 
 class BlogFilter(django_filters.FilterSet):
     category = django_filters.ChoiceFilter(choices=CATEGORIES_CHOICES)
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains', label='По заголовку')
 
     class Meta:
         model = Blog
-        fields = {
-            'title' : ['icontains'], 
-            'text': ['icontains'], 
-        }
+        fields = []
